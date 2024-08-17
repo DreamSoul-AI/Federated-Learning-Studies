6a5a1e2a9e245f7dfdf496d20ee52791c094324f
SwordXiaoJ <408555810@qq.com>
2024-08-17 09:51:41 -0700

    fix sampling

--- src/train_base_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="0" python train_model.py --init_seed 0 --num_experiments 1 --resume_mode 0 --control_name CIFAR10_cnn_250_1_40000_200_SGD-0.1-0.9-CosineAnnealingLR&
-wait
